f0b6b68cac08b512db854b81d6de7a83436084dc
pihhta <113553441+pihhta@users.noreply.github.com>
2025-03-11 16:57:19 -0400

    router2

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./app";
+import Dashboard from "./dashboard";
+import About from "./about";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {path:"/", element:<App/>},
+  {path:"/dashboard", element:<Dashboard/>},
+  {path:"/about", element:<About/>},
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
- <BrowserRouter>
-  <App />
-  </BrowserRouter>
+ 
+  <RouterProvider router={router}/>
+
   </React.StrictMode>,
 );
